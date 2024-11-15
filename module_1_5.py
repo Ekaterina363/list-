a93cc692175ce0d4250a86c3a1201806bc1320f5
Ekaterina363 <batsl363@gmail.com>
2024-11-15 19:59:27 +0400

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = tuple_ = 1, "cat", "32", "apple"
+print(immutable_var)
+print(type (immutable_var))
+#tuple_[1] = dog
+print(immutable_var)
+mutable_list = list_ = [23, "cat", True, "string"]
+print( mutable_list)
+print(type(mutable_list))
+list_[1] = "dog"
+print(mutable_list)
+
